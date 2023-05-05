b255de46da032ac5c559151f1abaad7deae5e8c3
elijah obara <elijahobara357@gmail.com>
2023-05-05 11:50:20 +0300

    fixed errors and task3

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -15,5 +15,17 @@ $(() => {
     checkedStr = checkedItems.map(item => item.name).join(', ');
     $('div.amenities h4').text(checkedStr);
   });
+
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/status/',
+    dataType: 'json',
+    success: data => {
+      if (data.status === 'OK') {
+        $('div#api_status').addClass('available');
+      } else {
+        $('div#api_status').removeClass('available');
+      }
+    }
+  });
 });
 
